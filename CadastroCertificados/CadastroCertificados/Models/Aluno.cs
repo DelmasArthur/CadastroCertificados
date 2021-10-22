e0f53eb36faa6c62a551125d10d501ea70a46e1e
DelmasArthur <arthurvdelmas@gmail.com>
2021-10-22 16:27:41 -0300

    Adição da Tabela de Alunos no Banco de Dados, Model Context para referenciar o banco de dados

---
+++ CadastroCertificados/CadastroCertificados/Models/Aluno.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace CadastroCertificados.Models
+{
+    public class Aluno
+    {
+        public int Id { get; set; }
+        public string NomeAluno { get; set; }
+        public string Curso { get; set; }
+    }
+}
---
+++ CadastroCertificados/CadastroCertificados/Models/Context.cs
@@ -0,0 +1,17 @@
+﻿using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace CadastroCertificados.Models
+{
+    public class Context : DbContext
+    {
+        public DbSet<Aluno> Alunos { get; set; }
+        protected override void OnConfiguring(DbContextOptionsBuilder optionBuilder)
+        {
+            optionBuilder.UseSqlServer(@"Server=(localdb)\mssqllocaldb;Database=CertificadosJIX;Integrated Security=True")
+        }
+    }
+}
