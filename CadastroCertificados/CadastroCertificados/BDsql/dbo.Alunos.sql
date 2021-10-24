115d55b96ed82c0170a20b0ffff0114155c9478d
DelmasArthur <arthurvdelmas@gmail.com>
2021-10-24 17:20:25 -0300

    Alterações no design das páginas - html e css/bootstrap

---
+++ CadastroCertificados/CadastroCertificados/BDsql/dbo.Alunos.sql
@@ -0,0 +1,7 @@
+﻿CREATE TABLE [dbo].[Alunos] (
+    [Id]        INT            IDENTITY (1, 1) NOT NULL,
+    [NomeAluno] NVARCHAR (MAX) NOT NULL,
+    [Curso]     NVARCHAR (MAX) NOT NULL,
+    CONSTRAINT [PK_Alunos] PRIMARY KEY CLUSTERED ([Id] ASC)
+);
+
---
+++ CadastroCertificados/CadastroCertificados/BDsql/dbo.Certificados.sql
@@ -0,0 +1,17 @@
+﻿CREATE TABLE [dbo].[Certificados] (
+    [Id]                INT            IDENTITY (1, 1) NOT NULL,
+    [NomeCertificado]   NVARCHAR (MAX) NOT NULL,
+    [AlunoId]           INT            NOT NULL,
+    [TipoCertificado]   NVARCHAR (MAX) NOT NULL,
+    [TempoCertificado]  INT           NOT NULL,
+    [LinkCertificado]   NVARCHAR (MAX) NOT NULL,
+    [StatusCertificado] NVARCHAR (MAX) NOT NULL,
+    CONSTRAINT [PK_Certificados] PRIMARY KEY CLUSTERED ([Id] ASC),
+    CONSTRAINT [FK_Certificados_Alunos_AlunoId] FOREIGN KEY ([AlunoId]) REFERENCES [dbo].[Alunos] ([Id]) ON DELETE CASCADE
+);
+
+
+GO
+CREATE NONCLUSTERED INDEX [IX_Certificados_AlunoId]
+    ON [dbo].[Certificados]([AlunoId] ASC);
+
